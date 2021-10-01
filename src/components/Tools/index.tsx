bcad195449460ae7e3664485f3c41a8e3b8bebe1
zero9527 <zero8297183@163.com>
2021-10-01 23:32:51 +0800

    feat: 组件统一大写命名

--- src/components/Tools/index.tsx
+++ src/components/Tools/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
 import ChangeTheme from '../ChangeTheme';
 import styles from './styles.less';
 
